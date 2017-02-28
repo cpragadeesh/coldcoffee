270516ab67477a5fed1cf56e41109e9ff679b27f
cpragadeesh <cpragadeesh@gmail.com>
2017-02-28 21:37:37 +0530

    added tests

---
+++ tests/validator_p1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	string input = argv[1];
+	string output = argv[2];
+
+	ifstream ifile, ofile;
+
+	ifile.open(input);
+	ofile.open(output);
+
+	int n;
+	int a, b;
+	int res;
+
+	ifile>>n;
+	for(int i = 1; i <= n; ++i) {
+		ifile>>a>>b;
+		ofile>>res;
+
+		if(res != a + b) {
+			return i;
+		}
+	}
+
+	return 0;
+}
